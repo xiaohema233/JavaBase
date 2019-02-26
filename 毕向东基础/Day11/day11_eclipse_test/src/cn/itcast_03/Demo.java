06133f2093ad25c83ebdaf2248e574af627ad7db
xiaohema233 <33052287+xiaohema233@users.noreply.github.com>
2019-02-26 16:21:13 +0800

    Day11
    Object类

---
+++ 毕向东基础/Day11/day11_eclipse_test/src/cn/itcast_03/Demo.java
@@ -0,0 +1,29 @@
+package cn.itcast_03;
+
+import cn.itcast_01.HelloWorld;
+
+/*
+ * alt+/ 内容辅助键
+ * 
+ * A:main方法
+ * 		main+alt+/ 回车即可
+ * B:输出语句
+ * 		syso+alt+/
+ * C:提示作用
+ * 		帮助你补齐一些你你不住的东西，还可以帮你起名字等。
+ */
+public class Demo {
+	// public static void main(String[] args) {
+	// System.out.println("HelloWorld");
+	// }
+
+	public static void main(String[] args) {
+		System.out.println();
+
+		String string = new String();
+
+		HelloWorld helloWorld = new HelloWorld();
+		
+		
+	}
+}
