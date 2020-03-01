62e8f09e4b49bbf2ddc3faa32d2a32b936b492d2
xiaohema233 <33052287+xiaohema233@users.noreply.github.com>
2020-03-01 13:00:48 +0800

    Day1 第一个JAVA入门程序

--- 毕向东基础/Day1/code/代码/HelloWorld案例/HelloWorld.java
+++ 毕向东基础/Day1/code/代码/HelloWorld案例/HelloWorld.java
@@ -1,8 +1,8 @@
 /**
- * HelloWorld
+ * 第一个JAVA入门程序 HelloWorld
  */
 class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("Hello World!");
 	}
 }
